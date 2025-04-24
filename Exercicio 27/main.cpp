5fc808564cbb0737f95907f48f985136c62b52a3
Will <164585530+WillKotlewski@users.noreply.github.com>
2025-04-23 21:55:32 -0300

    Add files via upload

---
+++ Exercicio 27/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese")
+    int n1,n2,n3;
+    cout << "Digite 3 números" << endl;
+    cin >> n1>>n2>>n3;
+    if(n1 > n2 && n1 > n3){
+        if(n2 > n3){
+
+        }
+    }
+    return 0;
+}
